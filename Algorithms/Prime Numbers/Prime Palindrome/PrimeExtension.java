63efbbbbe066143e50058df3c2d3a4335b0da7b1
s8189882 <109401577+s8189882@users.noreply.github.com>
2022-09-15 00:30:51 +0530

    Prime numbers that are Anagrams and Palindromes
    Extension of prime number program to find the prime numbers that are Anagram and Palindrome

---
+++ Algorithms/Prime Numbers/Prime Palindrome/PrimeExtension.java
@@ -0,0 +1,18 @@
+package algorithms.prime.anagram.palindrome;
+
+import java.util.ArrayList;
+
+public class PrimeExtension {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Prime Numbers Program!");
+		
+		PrimeAnagramPalindrome call = new PrimeAnagramPalindrome();
+		ArrayList<Integer> listOfPrimes = new ArrayList<>();
+		
+		call.generatePrime(listOfPrimes, 0, 1000);
+		ArrayList<Integer> listOfPrimeAnagrams = call.checkForAnagram(listOfPrimes);
+		ArrayList<Integer> listOfPrimePalindromes = call.checkForPalindrome(listOfPrimes);
+		call.checkForAnagramPalindrome(listOfPrimeAnagrams, listOfPrimePalindromes);
+	}
+}
