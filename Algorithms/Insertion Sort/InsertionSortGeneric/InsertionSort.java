5f7006ffaa5c6150f8f97b86b87957b5f6e76611
s8189882 <109401577+s8189882@users.noreply.github.com>
2022-09-15 00:25:15 +0530

    Insertion Sort for Generics Type

---
+++ Algorithms/Insertion Sort/InsertionSortGeneric/InsertionSort.java
@@ -0,0 +1,23 @@
+package sort.insertion;
+
+import java.util.ArrayList;
+
+public class InsertionSort {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Insertion Sort Program!\n");
+		InsertionSortGeneric genericObj = new InsertionSortGeneric<>();
+		
+		ArrayList<Integer> intList = new ArrayList();
+		genericObj.sortInt(intList);
+		
+		ArrayList<Double> doubleList = new ArrayList();
+		genericObj.sortDouble(doubleList);
+		
+		ArrayList<Integer> floatList = new ArrayList();
+		genericObj.sortFloat(floatList);
+		
+		ArrayList<Integer> stringList = new ArrayList();
+		genericObj.sortString(stringList);
+	}
+}
