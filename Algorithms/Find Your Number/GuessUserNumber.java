4b622191672e3bb5520e46d01565026144200675
s8189882 <109401577+s8189882@users.noreply.github.com>
2022-09-15 00:23:38 +0530

    Guess user's number
    Desc -> takes a command-line argument N, asks you to think of a number between 0 and N-1, where N = 2^n, and always guesses the answer with n questions.
    I/P -> the Number N and then recursively ask true/false if the number is between a high and low value
    O/P -> Print the intermediary number and the final answer

---
+++ Algorithms/Find Your Number/GuessUserNumber.java
@@ -0,0 +1,10 @@
+package algorithm.binarysearch.guessnumber;
+
+public class GuessUserNumber {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Number Guessing Program!\n");
+		FindNumber calls = new FindNumber();
+		calls.playWithUser();
+	}
+}
